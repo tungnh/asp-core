fa5e4de98f9ecb5c9a841c2b5ff0ff21cf2d3cbb
BachDX3 <bachdx3@rikkeisoft.com>
2024-10-17 17:29:25 +0900

    fix: name  method repository

--- OM.Application/Data/Repositories/IRepository.cs
+++ OM.Application/Data/Repositories/IRepository.cs
@@ -14,60 +14,60 @@ namespace OM.Application.Data.Repositories
         /// </summary>
         /// <param name="predicate"></param>
         /// <returns></returns>
-        Task<TEntity> SingleOrDefault(Expression<Func<TEntity, bool>> predicate);
+        Task<TEntity> SingleOrDefaultAsync(Expression<Func<TEntity, bool>> predicate);
 
         /// <summary>
         /// Gets all objects from database
         /// </summary>
-        Task<IEnumerable<TEntity>> FindAll();
+        Task<IEnumerable<TEntity>> FindAllAsync();
 
         /// <summary>
         /// Find object by Expression.
         /// </summary>
         /// <param name="predicate"></param>
         /// <returns></returns>
-        Task<IEnumerable<TEntity>> FindAll(Expression<Func<TEntity, bool>> predicate);
+        Task<IEnumerable<TEntity>> FindAllAsync(Expression<Func<TEntity, bool>> predicate);
 
         /// <summary>
         /// Create a new object to database.
         /// </summary>
         /// <param name="t">Specified a new object to create.</param>
-        Task<int> Add(TEntity t);
+        Task<int> AddAsync(TEntity t);
 
         /// <summary>
         /// Delete the object from database.
         /// </summary>
         /// <param name="t">Specified a existing object to delete.</param>        
-        Task<int> Delete(TEntity t);
+        Task<int> DeleteAsync(TEntity t);
 
         /// <summary>
         /// Delete the object from database.
         /// </summary>
         /// <param name="id">Specified a existing id object to delete.</param>        
-        Task<int> Delete(Guid id);
+        Task<int> DeleteAsync(Guid id);
 
         /// <summary>
         /// Update object changes and save to database.
         /// </summary>
         /// <param name="t">Specified the object to save.</param>
-        Task<int> Update(TEntity t);
+        Task<int> UpdateAsync(TEntity t);
 
         /// <summary>
         /// Update objects changes and save to database.
         /// </summary>
         /// <param name="entities">Specified the object to save.</param>
-        Task<int> AddRange(IEnumerable<TEntity> entities);
+        Task<int> AddRangeAsync(IEnumerable<TEntity> entities);
 
         /// <summary>
         /// Update objects changes and save to database.
         /// </summary>
         /// <param name="entities"></param>
-        Task<int> UpdateRange(IEnumerable<TEntity> entities);
+        Task<int> UpdateRangeAsync(IEnumerable<TEntity> entities);
 
         /// <summary>
         /// 
         /// </summary>
         /// <param name="entities"></param>
-        Task<int> RemoveRange(IEnumerable<TEntity> entities);
+        Task<int> RemoveRangeAsync(IEnumerable<TEntity> entities);
     }
 }
--- OM.Infrastructure/Data/Repositories/Repository.cs
+++ OM.Infrastructure/Data/Repositories/Repository.cs
@@ -18,26 +18,26 @@ namespace OM.Infrastructure.Data.Repositories
             _context = context;
         }
 
-        public async Task<int> Add(TEntity t)
+        public async Task<int> AddAsync(TEntity t)
         {
             await _context.Set<TEntity>().AddAsync(t);
             return await _context.SaveChangesAsync();
         }
 
-        public async Task<int> AddRange(IEnumerable<TEntity> entities)
+        public async Task<int> AddRangeAsync(IEnumerable<TEntity> entities)
         {
             await _context.Set<TEntity>().AddRangeAsync(entities);
             return await _context.SaveChangesAsync();
         }
 
-        public async Task<int> Delete(TEntity t)
+        public async Task<int> DeleteAsync(TEntity t)
         {
             _context.Attach(t);
             _context.Set<TEntity>().Remove(t);
             return await _context.SaveChangesAsync();
         }
 
-        public async Task<int> Delete(Guid id)
+        public async Task<int> DeleteAsync(Guid id)
         {
             var entityDelete = await _context.Set<TEntity>().FindAsync(id);
             _context.Attach(entityDelete);
@@ -45,35 +45,35 @@ namespace OM.Infrastructure.Data.Repositories
             return await _context.SaveChangesAsync();
         }
 
-        public async Task<IEnumerable<TEntity>> FindAll()
+        public async Task<IEnumerable<TEntity>> FindAllAsync()
         {
             return await _context.Set<TEntity>().AsNoTracking().ToListAsync(); 
         }
 
-        public async Task<IEnumerable<TEntity>> FindAll(Expression<Func<TEntity, bool>> predicate)
+        public async Task<IEnumerable<TEntity>> FindAllAsync(Expression<Func<TEntity, bool>> predicate)
         {
             return await _context.Set<TEntity>().Where(predicate).AsNoTracking().ToListAsync();
         }
 
-        public async Task<int> RemoveRange(IEnumerable<TEntity> entities)
+        public async Task<int> RemoveRangeAsync(IEnumerable<TEntity> entities)
         {
             _context.AttachRange(entities);
             _context.Set<TEntity>().RemoveRange(entities);
             return await _context.SaveChangesAsync();
         }
 
-        public async Task<TEntity> SingleOrDefault(Expression<Func<TEntity, bool>> predicate)
+        public async Task<TEntity> SingleOrDefaultAsync(Expression<Func<TEntity, bool>> predicate)
         {
             return await _context.Set<TEntity>().Where(predicate).SingleOrDefaultAsync();
         }
 
-        public async Task<int> Update(TEntity t)
+        public async Task<int> UpdateAsync(TEntity t)
         {
             _context.Update(t);
             return await _context.SaveChangesAsync();
         }
 
-        public async Task<int> UpdateRange(IEnumerable<TEntity> entities)
+        public async Task<int> UpdateRangeAsync(IEnumerable<TEntity> entities)
         {
             _context.UpdateRange(entities);
             return await _context.SaveChangesAsync();
